92160ece9a432d78135aff6cd3f401f06e448e55
Bruno Xavier <111466238+Bruno9407@users.noreply.github.com>
2023-11-21 19:18:44 -0300

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : AutoInstrucional.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Header.h"
+
+int main(void) {
+	setbuf(stdout, NULL);
+	menu();
+	return 0;
+}
